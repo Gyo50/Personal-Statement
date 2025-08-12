52e1c3793121972a1994ef92a05132084995da41
gyo <oey157@nate.com>
2025-08-12 17:03:07 +0900

    myself

--- src/test/data/myself.js
+++ src/test/data/myself.js
@@ -6,21 +6,31 @@ import myimg from './dataimg/myimg.jpg';
 export default function Myself() {
   return (
     <>
-      <div className="flex mt-10 justify-around items-center">
-        <div>
-          <img src={myimg} alt="내 사진" className="w-48 h-auto rounded-3xl p-4" />
-        </div>
-        <div className='border-double'>
+      <div className='flex mt-10'>
+        <div className="basis-1/2 flex flex-col justify-between">
           <div className=''>
-            <p>이름: 오영교</p>
-            <p>생년월일: 1999년 8월 22일 (만 25세)</p>
-            <p>전화번호: 010 - 8513 - 0528</p>
-            <p>이메일: oey157@nate.com</p>
-            <p>깃허브 주소: <a className='text-' href='https://github.com/Gyo50'>https://github.com/Gyo50</a></p>
+            <img src={myimg} className='w-48 h-auto rounded-3xl' alt="내 사진"/>
+          </div>
+          <div className='border-double'>
+            <div className=''>
+              <p className='myself_p'><span className='p_title'>이름</span> <br/>오영교</p>
+              <p className='myself_p'><span className='p_title'>생년월일</span> <br/>1999년 8월 22일 (만 25세)</p>
+              <p className='myself_p'><span className='p_title'>전화번호</span> <br/>010 - 8513 - 0528</p>
+              <p className='myself_p'><span className='p_title'>이메일</span> <br/>oey157@nate.com</p>
+              <p className='myself_p'><span className='p_title'>깃허브 주소</span> <br/><a className='underline' href='https://github.com/Gyo50'>https://github.com/Gyo50</a></p>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
+        <div className='basis-1/2'>
+          <p className='myself_p'><span className='p_title'>학력</span> <br/>국제대학교 전문학사 2018 ~ 2023 <br/>동양미래대학교 학사 2023 ~ 2024 </p>
+          <p className='myself_p'><span className='p_title'>별명</span> <br/>소프트영 = 부드러운 성격과 학과명이 합쳐진 별명입니다</p>
+          <p className='myself_p'><span className='p_title'>MBTI</span> <br/>ESFJ</p>
+          <p className='myself_p'><span className='p_title'>성격</span> <br/>항상 긍정적이고 신중하게 계획하기 책임감을 느끼면서 꾸준히 노력하기</p>
+          <p className='myself_p'><span className='p_title'>가치관</span> <br/>oey157@nate.com</p>
+          <p className='myself_p'><span className='p_title'>인생모토</span> <br/>직업에서 행복을 찾아라 아니면 행복이 무엇인지 절대 모를것이다</p>
+          <p className='myself_p'><span className='p_title'>취미</span> <br/>취미, 운동, 독서</p>
+          <p className='myself_p'><span className='p_title'>목표</span> <br/>성장하고 배우면서 기억에 남는 사람이 되는 것</p>
+        </div>
       </div>
     </>
   )
--- src/test/data/myskill.js
+++ src/test/data/myskill.js
@@ -3,56 +3,54 @@ import React from 'react'
 export default function Myskill() {
     return (
         <>
+
+            <p><strong>Frontend</strong></p>
             <div>
-                <ul>
-                    <li>⚛️ React / Next.js</li>
-                    <li>🎨 TailwindCSS / Styled-components</li>
-                    <li>🧠 TypeScript / JavaScript</li>
-                    <li>🌐 WebGL / Three.js</li>
-                    <li>🛠️ Git / GitHub / CI</li>
-                </ul>
-                <p style={{ marginTop: "10px" }}>위 기술들을 활용해 반응형 인터페이스와 인터랙티브 웹을 구현할 수 있습니다.</p>
+                <img src="https://img.shields.io/badge/html5-E34F26?style=flat-square&logo=html5&logoColor=white" />
+                <img src="https://img.shields.io/badge/css-1572B6?style=flat-square&logo=css3&logoColor=white" />
+                <img src="https://img.shields.io/badge/javascript-F7DF1E?style=flat-square&logo=javascript&logoColor=black" />
+                <img src="https://img.shields.io/badge/React-61DAFB?style=flat-square&logo=react&logoColor=black" />
+                <img src="https://img.shields.io/badge/JQuery-0769AD?style=flat-square&logo=jquery&logoColor=white" />
+                <img src="https://img.shields.io/badge/TypeScript-3178C6?style=flat-square&logo=typescript&logoColor=white" />
             </div>
+
+            <p><strong>Backend</strong></p>
             <div>
-                <ul>
-                    <li>⚛️ React / Next.js</li>
-                    <li>🎨 TailwindCSS / Styled-components</li>
-                    <li>🧠 TypeScript / JavaScript</li>
-                    <li>🌐 WebGL / Three.js</li>
-                    <li>🛠️ Git / GitHub / CI</li>
-                </ul>
-                <p style={{ marginTop: "10px" }}>위 기술들을 활용해 반응형 인터페이스와 인터랙티브 웹을 구현할 수 있습니다.</p>
+                <img src="https://img.shields.io/badge/Java-007396?style=for-the-badge&logo=Java&logoColor=white" />
+                <img src="https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=node.js&logoColor=white" />
+                <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" />
+                <img src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white" />
             </div>
+            <p><strong>Database</strong></p>
             <div>
-                <ul>
-                    <li>⚛️ React / Next.js</li>
-                    <li>🎨 TailwindCSS / Styled-components</li>
-                    <li>🧠 TypeScript / JavaScript</li>
-                    <li>🌐 WebGL / Three.js</li>
-                    <li>🛠️ Git / GitHub / CI</li>
-                </ul>
-                <p style={{ marginTop: "10px" }}>위 기술들을 활용해 반응형 인터페이스와 인터랙티브 웹을 구현할 수 있습니다.</p>
+                <img src="https://img.shields.io/badge/mysql-4479A1?style=for-the-badge&logo=mysql&logoColor=white" />
+                <img src="https://img.shields.io/badge/-MongoDB-13aa52?style=for-the-badge&logo=mongodb&logoColor=white" />
             </div>
+            <p><strong>Server</strong></p>
             <div>
-                <ul>
-                    <li>⚛️ React / Next.js</li>
-                    <li>🎨 TailwindCSS / Styled-components</li>
-                    <li>🧠 TypeScript / JavaScript</li>
-                    <li>🌐 WebGL / Three.js</li>
-                    <li>🛠️ Git / GitHub / CI</li>
-                </ul>
-                <p style={{ marginTop: "10px" }}>위 기술들을 활용해 반응형 인터페이스와 인터랙티브 웹을 구현할 수 있습니다.</p>
+                <img src="https://img.shields.io/badge/linux-FCC624?style=for-the-badge&logo=linux&logoColor=black" />
             </div>
+            <p><strong>Development Tools</strong></p>
             <div>
-                <ul>
-                    <li>⚛️ React / Next.js</li>
-                    <li>🎨 TailwindCSS / Styled-components</li>
-                    <li>🧠 TypeScript / JavaScript</li>
-                    <li>🌐 WebGL / Three.js</li>
-                    <li>🛠️ Git / GitHub / CI</li>
-                </ul>
-                <p style={{ marginTop: "10px" }}>위 기술들을 활용해 반응형 인터페이스와 인터랙티브 웹을 구현할 수 있습니다.</p>
+                <img src="https://img.shields.io/badge/Visual Studio Code-007ACC?style=flat-square&logo=visual-studio-code&logoColor=white" />
+                <img src="https://img.shields.io/badge/Visual Studio-5C2D91?style=flat-square&logo=visual-studio&logoColor=white" />
+                <img src="https://img.shields.io/badge/Eclipse IDE-2C2255?style=flat-square&logo=eclipse-ide&logoColor=white" />
+                <img src="https://img.shields.io/badge/Anaconda-44A833?style=flat-square&logo=anaconda&logoColor=white" />
+                <img src="https://img.shields.io/badge/Android Studio-3DDC84?style=flat-square&logo=Android Studio&logoColor=white" />
+                <img src="https://img.shields.io/badge/Jupyter%20Notebook-F37626?style=flat-square&logo=jupyter&logoColor=white" />
             </div>
+            <p><strong>Version Control</strong></p>
+            <div>
+                <img src="https://img.shields.io/badge/Git-F05032?style=flat-square&logo=git&logoColor=white" />
+                <img src="https://img.shields.io/badge/GitHub-181717?style=flat-square&logo=github&logoColor=white" />
+            </div>
+            <p><strong>Communication</strong></p>
+            <div>
+                <img src="https://img.shields.io/badge/Figma-F24E1E?style=flat-square&logo=figma&logoColor=white" />
+                <img src="https://img.shields.io/badge/Adobe%20Photoshop-%231e1e1e?style=flat-square&logo=adobephotoshop&logoColor=white
+" />
+            </div>
+
         </>
     )
 }
